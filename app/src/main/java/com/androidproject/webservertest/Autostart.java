b2b7864e2db05a82bfce3ed21db18f2f60227c5e
suryadi_osjk <lisyandisuryadi@gmail.com>
2020-07-07 15:27:46 +0800

    Autostart

---
+++ app/src/main/java/com/androidproject/webservertest/Autostart.java
@@ -0,0 +1,23 @@
+package com.androidproject.webservertest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+import android.widget.Toast;
+
+public class Autostart extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent arg1) {
+        // TODO Auto-generated method stub
+        Log.w("log_webservertest", "starting service from boot completed");
+        Toast.makeText(context,"Service Started", Toast.LENGTH_SHORT).show();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(new Intent(context, TestServices.class));
+        } else {
+            context.startService(new Intent(context, TestServices.class));
+        }
+    }
+}
